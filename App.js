c5e05e974763ab0c78595921e0905c21fac17d9b
Morgan Fogarty <mofo37@gmail.com>
2017-12-12 14:29:09 -1000

    first react-native boiler plate

---
+++ App.js
@@ -0,0 +1,30 @@
+'use strict';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+export default class App extends Component<{}> {
+  render() {
+     return React.createElement(Text, {style: styles.description}, "Search for houses to buy!"
+     );
+  }
+}
+
+const styles = StyleSheet.create({
+  description: {
+    fontSize: 18,
+    textAlign: 'center',
+    color: '#656565',
+    marginTop: 65,
+  },
+});
