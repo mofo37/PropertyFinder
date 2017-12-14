5cde7caee85fc1b95f316ff95bdcf8180a822688
Morgan Fogarty <mofo37@gmail.com>
2017-12-14 12:21:58 -1000

    Set initial route for SearchPage component

--- App.js
+++ App.js
@@ -10,13 +10,26 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  NavigatorIOS
 } from 'react-native';
+
+class SearchPage extends Component<{}> {
+  render() {
+    return <Text style={styles.description}>Search for houses to buy!</Text>;
+  }
+}
 
 export default class App extends Component<{}> {
   render() {
-     return React.createElement(Text, {style: styles.description}, "Search for houses to buy!"
-     );
+    return (
+      <NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'Property Finder',
+          component: SearchPage,
+        }} />
+    );
   }
 }
 
@@ -26,5 +39,8 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: '#656565',
     marginTop: 65,
+  },
+  container: {
+    flex: 1,
   },
 });
